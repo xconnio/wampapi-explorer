9ac4ecd1e21793fcc28ffe53fc744b42abbf24d0
Ismail Akram <rubyonrails3@gmail.com>
2025-09-25 19:26:52 +0500

    Adds schema input field and link as query parameter

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -19,9 +19,8 @@ export default defineConfig({
       define: {
         global: 'globalThis',
       },
-    }
+    },
   },
-  base: '/wampapi-explorer/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
